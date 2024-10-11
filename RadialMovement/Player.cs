5fb5637f8a87c8dc6deae6b1f5c1b51293b11277
Kaspar Moberg <kaspar@kasparm.home>
2024-10-11 22:56:55 +0200

    feat(radialmovement): finished radial movement

---
+++ RadialMovement/Player.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	private Camera2D _camera;
+
+	public override void _Ready()
+	{
+		FloorMaxAngle = 90;
+
+		_camera = GetNodeOrNull<Camera2D>("Camera");
+		if (_camera == null) {
+			GD.PrintErr("No camera attached to player...");
+			throw new MissingMemberException("Camera missing on player");
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = GlobalTransform.BasisXformInv(Velocity);
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		_camera.Rotate(_camera.GetAngleTo(Global.world.GlobalPosition) - Mathf.Pi/2);
+
+		Velocity = GlobalTransform.BasisXform(velocity);
+		MoveAndSlide();
+	}
+}
