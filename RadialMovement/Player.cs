a541a469aeb31b586dd5d251260e7ab2c94fdb9d
Kaspar Moberg <kaspeti123@gmail.com>
2024-10-12 12:18:28 +0200

    refactor(radialmovement): pulled camera logic to own script

--- RadialMovement/Player.cs
+++ RadialMovement/Player.cs
@@ -6,23 +6,15 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
-	private Camera2D _camera;
 	private AnimatedSprite2D _sprite;
 
 	public override void _Ready()
 	{
 		FloorMaxAngle = 45;
 
-		_camera = GetNodeOrNull<Camera2D>("/root/Main/Camera");
-		if (_camera == null) {
-			GD.PrintErr("No Camera with name 'Camera' in main scene...");
-			throw new MissingMemberException("Camera missing in main scene");
-		}
-
 		_sprite = GetNodeOrNull<AnimatedSprite2D>("Sprite");
 		if (_sprite == null) {
 			GD.PrintErr("No AnimatedSprite2D with name 'Sprite' attached to player...");
-			throw new MissingMemberException("AnimatedSprite2D missing on player");
 		}
 	}
 
@@ -58,10 +50,6 @@ public partial class Player : CharacterBody2D
 			// Set idle animation
 			_sprite.Play("idle");
 		}
-
-		// Set position and rotation of camera
-		_camera.Rotate(_camera.GetAngleTo(Global.world.GlobalPosition) - Mathf.Pi/2);
-		_camera.Position = Position;
 
 		Velocity = GlobalTransform.BasisXform(velocity);
 		MoveAndSlide();
