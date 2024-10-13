2076d54349ec3b1d0bfaa7bf2a537919672754fd
Kaspar Moberg <kaspeti123@gmail.com>
2024-10-13 16:34:28 +0200

    feat: more splosions

--- Splosion/Explosion.cs
+++ Splosion/Explosion.cs
@@ -3,13 +3,21 @@ using System;
 
 public partial class Explosion : Node2D
 {
+	[ExportGroup("Explosion Settings")]
 	[Export]
 	private Texture2D[] _explosionTextures;
 	[Export]
 	private float _explosionSpeed;
 
+	[Export]
+	private AudioStreamMP3[] _sounds;
+
+	private AudioStreamPlayer2D _explosionAudio;
+
 	private Sprite2D[] _explosionSprites;
 	private Vector2[] _directions;
+
+	private AnimatedSprite2D _cloudSprite;
 
 	private bool _isExploding = false;
 	
@@ -24,10 +32,13 @@ public partial class Explosion : Node2D
 
 			_explosionSprites[i] = sprite;
 			_directions[i] = new Vector2(GD.RandRange(-100, 100) / 100.0f, GD.RandRange(-100, 100) / 100.0f).Normalized(); 
-			GD.Print(_directions[i]);
 		}
 
 		_isExploding = true;
+		_explosionAudio.Stream = _sounds[GD.Randi() % _sounds.Length];
+		_explosionAudio.Play();
+		_cloudSprite.Visible = true;
+		_cloudSprite.Play("explode");
 		_deleteTimer.Start();
 	}
 
@@ -42,9 +53,25 @@ public partial class Explosion : Node2D
 			return;
 		}
 
+		_explosionAudio = GetNodeOrNull<AudioStreamPlayer2D>("Sound");
+		if (_explosionAudio == null) {
+			GD.PrintErr("No Sound on Explosion node...");
+			QueueFree();
+			return;
+		}
+
+		_cloudSprite = GetNodeOrNull<AnimatedSprite2D>("Cloud");
+		if (_cloudSprite == null) {
+			GD.PrintErr("No Cloud on Explosion node...");
+			QueueFree();
+			return;
+		}
+		_cloudSprite.AnimationFinished += () => _cloudSprite.Visible = false;
+
 		_deleteTimer.Timeout += QueueFree;
 
 		GD.Randomize();
+		Explode();
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
@@ -52,6 +79,8 @@ public partial class Explosion : Node2D
 			for (var i = 0; i < _explosionSprites.Length; i++) {
 				var dir = _directions[i];
 				var sprite = _explosionSprites[i];
+				GD.Print(_deleteTimer.TimeLeft);
+				sprite.Modulate = new Color(1, 1, 1, 1.34f * (float)_deleteTimer.TimeLeft);
 
 				sprite.Position += dir * _explosionSpeed * (float)delta;
 			}
